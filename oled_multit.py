91480b974f6f3da83dbe2445c34c93c9ab5c7609
lance36 <lance36@gmail.com>
2017-12-24 02:08:03 +0100

    got banned from the api lulz.

--- oled_multit.py
+++ oled_multit.py
@@ -12,7 +12,7 @@ import time,datetime
 import Adafruit_GPIO.SPI as SPI
 import RPi.GPIO as GPIO
 import Adafruit_SSD1306
-import json, urllib
+import json, urllib, requests
 import os
 import netifaces as ni
 
@@ -80,11 +80,11 @@ bottom = height-padding
 x = 0
 
 
-# Load default font.
+# Load fonts.
 font = ImageFont.load_default()
 font35 = ImageFont.truetype('vcr.ttf', 35)
 
-#Variables cause 2 unsynchroned threads
+#Variables cause 2 threads ?
 flip = "0"
 mode = 0
 BTC = ""
@@ -92,7 +92,7 @@ IP = ""
 CPU = ""
 MemUsage = ""
 Disk = ""
-timer = 27 # on my system atleast?
+timer = 30 
 kill = 0
 
 class varupdate(threading.Thread):
@@ -116,30 +116,34 @@ class varupdate(threading.Thread):
 		while True:
 			if mode ==1:
 				c.acquire()
-				#cmd = "hostname -I | cut -d\' \' -f1"
-				#IP = subprocess.check_output(cmd, shell = True )
 				IP = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
 				CPU = "Cpu: " + str(psutil.cpu_percent()) + "% "
 				Mem = psutil.virtual_memory() 
 				MemUsage = "Mem: "  + str(Mem.used >> 20) + "/" + str(Mem.total >> 20) + " " + str(Mem.percent) + "%"
-				cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-				Disk = subprocess.check_output(cmd, shell = True )
+				# cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+				# Disk = subprocess.check_output(cmd, shell = True )
 				url = "http://api.coindesk.com/v1/bpi/currentprice.json"
-				BTC = '{0:,}'.format( int(json.load(urllib.urlopen(url))['bpi']['USD']['rate_float']) )
+				headers = {
+					'User-Agent': 'AppleWebKit/537.36 (KHTML, like Gecko) ',
+					'Accept': 'application/json'}
+				BTC = '{0:,}'.format( int(requests.get(url, headers=headers).json()['bpi']['USD']['rate_float']) )
 				c.release()
-				timer=30
-				for i in range(30):
+				timer=500
+				for i in range(500):
 					time.sleep(1)
 					if kill == 1:
 						return
 
 			else:
 				url = "http://api.coindesk.com/v1/bpi/currentprice.json"
+				headers = {
+					'User-Agent': 'AppleWebKit/537.36 (KHTML, like Gecko) ',
+					'Accept': 'application/json'}
 				c.acquire()
-				BTC = int(json.load(urllib.urlopen(url))['bpi']['USD']['rate_float'])
+				BTC = '{0:,}'.format( int(requests.get(url, headers=headers).json()['bpi']['USD']['rate_float']) )
 				c.release()
-				timer=30
-				for i in range(30):
+				timer=500
+				for i in range(500):
 					time.sleep(1)
 					if kill == 1:
 						return
@@ -181,7 +185,7 @@ class screenctl(threading.Thread):
 					draw.text((x, top),	   "IP: " + str(IP),  font=font, fill=255)
 					draw.text((x, top+8),	 str(CPU), font=font, fill=255)
 					draw.text((x, top+16),	str(MemUsage),  font=font, fill=255)
-					draw.text((x, top+24),	str(Disk),  font=font, fill=255)
+					# draw.text((x, top+24),	str(Disk),  font=font, fill=255)
 					draw.text((x, top+25),	"______________________s	   ",  font=font, fill=255)
 					draw.text((x, top+25),	"______________________	   ",  font=font, fill=255)
 					draw.text((x, top+48),	"USD\BTC: "+ str(BTC),  font=font, fill=255)
@@ -241,7 +245,6 @@ while True:
 	try:
 	#Keep main thread from exiting, trying to exit threads correctly...
 		time.sleep(1)
-		print "sleepin " + str(kill)
 		if kill == 1:
 			bye(disp)
 			kill = 1
